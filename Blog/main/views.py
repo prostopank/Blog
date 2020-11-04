40466fbe1a05b3090956456aef61f6771020924d
ptostopank <52743103+ptostopank@users.noreply.github.com>
2020-11-04 15:02:34 +0300

    update layout

--- Blog/main/views.py
+++ Blog/main/views.py
@@ -5,3 +5,7 @@ def index(request):
     users = User.objects.all()
     return render(request, 'main/index.html', {'title':'All users', 'users': users})
 
+def sign_in(reques):
+    
+    return render(reques, 'main/signin.html')
+
