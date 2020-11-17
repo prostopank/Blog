8fecd6141be4c8d24fc509bc89feb75546f900a2
ptostopank <52743103+ptostopank@users.noreply.github.com>
2020-11-17 11:38:36 +0300

    testing urls

--- Blog/main/tests.py
+++ Blog/main/tests.py
@@ -1,14 +1,16 @@
+from django.conf.urls import url
 from django.http import response
-from django.test import TestCase, Client, RequestFactory
+from django.test import TestCase, Client, RequestFactory, SimpleTestCase
 from .models import Article, Comments
 from django.contrib.auth import get_user_model
-from django.urls import reverse
-import json
+from django.urls import reverse, resolve, Resolver404
+from . import views
+
 
 User = get_user_model()
 
 
-class BlogTestViewsCases(TestCase):
+"""class BlogTestViewsCases(TestCase):
 
     def setUp(self) -> None:
         self.client = Client()
@@ -52,3 +54,58 @@ class BlogTestViewsCases(TestCase):
 
     def test_login_user(self):
         login = self.client.login(username='testuser', password='password')
+"""
+
+class TestUrls(SimpleTestCase):
+
+    def test_index_url_is_resolves(self):
+        url = reverse('index')
+        self.assertEquals(resolve(url).func.__name__, views.HomeListView.as_view().__name__)
+
+    def test_article_url_is_resolves(self):
+        url = reverse('article', args=['1'])
+        self.assertEquals(resolve(url).func.__name__, views.ArticleDetail.as_view().__name__)
+
+    def test_editpage_url_is_resolves(self):
+        url = reverse('editpage')
+        self.assertEquals(resolve(url).func.__name__, views.ArticleEditView.as_view().__name__)
+        
+    def test_updatepage_url_is_resolves(self):
+        url = reverse('updatepage', args=['1'])
+        self.assertEquals(resolve(url).func.__name__, views.ArticleUpdateView.as_view().__name__)
+    
+    def test_deletepage_url_is_resolves(self):
+        url = reverse('deletepage', args=['1'])
+        self.assertEquals(resolve(url).func.__name__, views.ArticleDeleteView.as_view().__name__)
+    
+    def test_login_url_is_resolves(self):
+        url = reverse('login')
+        self.assertEquals(resolve(url).func.__name__, views.UserLoginView.as_view().__name__)
+
+    def test_register_url_is_resolves(self):
+        url = reverse('register')
+        self.assertEquals(resolve(url).func.__name__, views.RegisterUserView.as_view().__name__)
+    
+    def test_logout_url_is_resolves(self):
+        url = reverse('logout')
+        self.assertEquals(resolve(url).func.__name__, views.UserLogoutView.as_view().__name__)
+    
+    def test_logout_url_is_resolves(self):
+        url = reverse('logout')
+        self.assertEquals(resolve(url).func.__name__, views.UserLogoutView.as_view().__name__)
+    
+    def test_search_url_is_resolves(self):
+        url = reverse('search')
+        self.assertEquals(resolve(url).func.__name__, views.SearchView.as_view().__name__)
+    
+    def test_favorite_article_url_is_resolves(self):
+        url = reverse('favoritearticle')
+        self.assertEquals(resolve(url).func.__name__, views.FavoriteView.as_view().__name__)
+    
+    def test_add_favorite_article_url_is_resolves(self):
+        url = reverse('addfavorite', args=['1'])
+        self.assertEquals(resolve(url).func.__name__, views.AddToFavoriteView.as_view().__name__)
+    
+    
+
+    
