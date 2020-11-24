ab967b37e4c7771abcab4fa7a5f72a5afb5c46b3
ptostopank <52743103+ptostopank@users.noreply.github.com>
2020-11-24 14:17:03 +0300

    up

--- Blog/main/forms.py
+++ Blog/main/forms.py
@@ -3,45 +3,53 @@ from .models import Article, Comments
 from django.contrib.auth.forms import AuthenticationForm
 from django.contrib.auth.models import User
 
+
 class ArticleForm(forms.ModelForm):
     class Meta:
         model = Article
-        fields = ('title','body')
+        fields = ('title', 'body')
+
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         for field in self.fields:
             self.fields[field].widget.attrs['class'] = 'form-control'
+
 
 class LoginUserForm(AuthenticationForm, forms.ModelForm):
     class Meta:
         model = User
-        fields = ('username','password')
+        fields = ('username', 'password')
+
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         for field in self.fields:
             self.fields[field].widget.attrs['class'] = 'form-control'
 
+
 class RegisterUserForm(forms.ModelForm):
     class Meta:
         model = User
-        fields = ('username','password')
+        fields = ('username', 'password')
+
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         for field in self.fields:
-            self.fields[field].widget.attrs['class'] = 'form-control'    
-    def save(self, commit = True):
+            self.fields[field].widget.attrs['class'] = 'form-control'
+
+    def save(self, commit=True):
         user = super().save(commit=False)
         user.set_password(self.cleaned_data["password"])
         if commit:
             user.save()
         return user
 
+
 class CommentForm(forms.ModelForm):
     class Meta:
         model = Comments
         fields = fields = ('body',)
+
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         for field in self.fields:
             self.fields[field].widget.attrs['class'] = 'form-control'
-        
