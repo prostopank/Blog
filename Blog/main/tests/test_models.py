5375f64a12202be9483d2b7a4dca6dea85ba69d7
ptostopank <52743103+ptostopank@users.noreply.github.com>
2020-11-24 14:47:29 +0300

    Update test_models.py

--- Blog/main/tests/test_models.py
+++ Blog/main/tests/test_models.py
@@ -1,16 +1,15 @@
-from main.models import Article, Comments
+from ..models import Article, Comments
 from django.test import TestCase
 from datetime import datetime
 from django.contrib.auth.models import User
-from django.contrib.auth import get_user_model
-User = get_user_model()
+
 
 class TestModels(TestCase):
     def setUp(self) -> None:
         self.user = User.objects.create(username='testuser', password='password')
         self.article = Article.objects.create(user_id=self.user, title='testTitle', body='testBody')
         self.comment = Comments.objects.create(user_id=self.user, article_id=self.article, body='testComment')
-    
+
     def test_article_fields(self):
         self.assertIsInstance(self.article.title, str)
         self.assertIsInstance(self.article.body, str)
