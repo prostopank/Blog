ba137ca91bec2f4e8c84a4a9135fe359367943d2
ptostopank <52743103+ptostopank@users.noreply.github.com>
2020-11-24 15:27:29 +0300

    Update test_forms.py

--- Blog/main/tests/test_forms.py
+++ Blog/main/tests/test_forms.py
@@ -15,19 +15,23 @@ class TestForms(TestCase):
 
     def test_article_form_no_data(self):
         form = forms.ArticleForm(data={})
+
         self.assertFalse(form.is_valid())
         self.assertEquals(len(form.errors), 2)
 
     def test_user_login_form_valid_data(self):
         User.objects.create_user(username='testuser', password='password')
+
         form = forms.LoginUserForm(data={
             'username': 'testuser',
             'password': 'password',
         })
+
         self.assertTrue(form.is_valid())
 
     def test_user_login_form_no_data(self):
         form = forms.LoginUserForm(data={})
+
         self.assertFalse(form.is_valid())
         self.assertEquals(len(form.errors), 2)
 
@@ -36,10 +40,12 @@ class TestForms(TestCase):
             'username': 'testuser',
             'password': 'password',
         })
+
         self.assertTrue(form.is_valid())
 
     def test_user_register_form_no_data(self):
         form = forms.RegisterUserForm(data={})
+
         self.assertFalse(form.is_valid())
         self.assertEquals(len(form.errors), 2)
 
@@ -52,5 +58,6 @@ class TestForms(TestCase):
 
     def test_comment_form_no_data(self):
         form = forms.RegisterUserForm(data={})
+        
         self.assertFalse(form.is_valid())
         self.assertEquals(len(form.errors), 2)
