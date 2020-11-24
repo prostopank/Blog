5e6066bf4af1c89214f70fa3f5e1c4318393d3b5
ptostopank <52743103+ptostopank@users.noreply.github.com>
2020-11-24 15:24:45 +0300

    Update test_models.py

--- Blog/main/tests/test_models.py
+++ Blog/main/tests/test_models.py
@@ -1,4 +1,4 @@
-from ..models import Article, Comments
+from ..models import Article, Comments, FavoriteArticle
 from django.test import TestCase
 from datetime import datetime
 from django.contrib.auth.models import User
@@ -9,12 +9,17 @@ class TestModels(TestCase):
         self.user = User.objects.create(username='testuser', password='password')
         self.article = Article.objects.create(user_id=self.user, title='testTitle', body='testBody')
         self.comment = Comments.objects.create(user_id=self.user, article_id=self.article, body='testComment')
+        self.favorite_article = FavoriteArticle.objects.create(user_id=self.user, article_id=self.article)
 
-    def test_article_fields(self):
+    def test_article_model(self):
         self.assertIsInstance(self.article.title, str)
         self.assertIsInstance(self.article.body, str)
         self.assertIsInstance(self.article.create_date, datetime)
+        self.assertEquals(str(self.article), self.article.title)
 
-    def test_comments_fields(self):
+    def test_comments_model(self):
         self.assertIsInstance(self.comment.body, str)
         self.assertIsInstance(self.comment.create_date, datetime)
+
+    def test_favorite_article_model(self):
+        self.assertEquals(str(self.favorite_article), str(self.favorite_article.id))
